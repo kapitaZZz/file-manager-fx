b67dafa2485c792bb4c92069cfcd818422bb5988
Anton Garussky <agarussky@gmail.com>
2021-11-05 15:18:29 +0300

    add FileInfo class, modified view of FM, add filename and filetype columns

---
+++ src/main/java/dead/sec/kapitaz/FileManager/FileInfo.java
@@ -0,0 +1,52 @@
+package dead.sec.kapitaz.FileManager;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+
+@Getter
+@Setter
+public class FileInfo {
+    public enum FileType {
+        FILE("F"), DIRECTORY("D");
+
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        FileType(String name) {
+            this.name = name;
+        }
+    }
+
+    private String fileName;
+    private FileType type;
+    private long size;
+    private LocalDateTime lastModified;
+
+    public FileInfo(Path path) {
+        try {
+            this.fileName = path.getFileName().toString();
+            this.size = Files.size(path);
+            this.type = Files.isDirectory(path) ? FileType.DIRECTORY : FileType.FILE;
+
+            if (this.type == FileType.DIRECTORY) {
+                this.size = -1L;
+            }
+
+            this.lastModified = LocalDateTime.ofInstant(Files.getLastModifiedTime(path).toInstant(),
+                    ZoneOffset.ofHours(0));
+
+        } catch (IOException e) {
+            throw new RuntimeException("Unable to create file info from path.");
+        }
+    }
+}
