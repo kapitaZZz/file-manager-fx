bc3e2029a7ac17d34abd9076b365838e598799a4
Anton Garussky <agarussky@gmail.com>
2021-11-05 21:05:44 +0300

    add copy files function

--- src/main/java/dead/sec/kapitaz/FileManager/Controller.java
+++ src/main/java/dead/sec/kapitaz/FileManager/Controller.java
@@ -1,55 +1,53 @@
 package dead.sec.kapitaz.FileManager;
 
 import javafx.application.Platform;
-import javafx.beans.property.SimpleStringProperty;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
-
 import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.layout.VBox;
 
 import java.io.IOException;
-import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ResourceBundle;
-import java.util.stream.Collectors;
 
-public class Controller implements Initializable {
+public class Controller {
     @FXML
-    TableView<FileInfo> filesTable;
-
+    VBox leftPanel, rightPanel;
 
     public void btnExitAction(ActionEvent actionEvent) {
         Platform.exit();
     }
 
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
-        TableColumn<FileInfo, String> fileTypeColumn = new TableColumn<>();
-        fileTypeColumn.setCellValueFactory(param -> new SimpleStringProperty(param.getValue().getType().getName()));
-        fileTypeColumn.setPrefWidth(24);
+    public void copyBtnAction(ActionEvent actionEvent) {
+        PanelController leftPC = (PanelController) leftPanel.getProperties().get("ctrl");
+        PanelController rightPC = (PanelController) rightPanel.getProperties().get("ctrl");
 
-        TableColumn<FileInfo, String> fileNameColumn = new TableColumn<>("File Name");
-        fileNameColumn.setCellValueFactory(param -> new SimpleStringProperty(param.getValue().getFileName()));
-        fileNameColumn.setPrefWidth(240);
+        if (leftPC.getSelectedFilename() == null && rightPC.getSelectedFilename() == null) {
+            Alert alert = new Alert(Alert.AlertType.ERROR, "Ни один файл не был выбран", ButtonType.OK);
+            alert.showAndWait();
+            return;
+        }
 
-        filesTable.getColumns().addAll(fileTypeColumn, fileNameColumn);
+        PanelController srcPC = null, dstPC = null;
+        if (leftPC.getSelectedFilename() != null) {
+            srcPC = leftPC;
+            dstPC = rightPC;
+        }
+        if (rightPC.getSelectedFilename() != null) {
+            srcPC = rightPC;
+            dstPC = leftPC;
+        }
 
-        updateList(Paths.get("."));
-    }
+        Path srcPath = Paths.get(srcPC.getCurrentPath(), srcPC.getSelectedFilename());
+        Path dstPath = Paths.get(dstPC.getCurrentPath()).resolve(srcPath.getFileName().toString());
 
-    public void updateList(Path path) {
-        filesTable.getItems().clear();
         try {
-            filesTable.getItems().addAll(Files.list(path).map(FileInfo::new).collect(Collectors.toList()));
-            filesTable.sort();
+            Files.copy(srcPath, dstPath);
+            dstPC.updateList(Paths.get(dstPC.getCurrentPath()));
         } catch (IOException e) {
-            Alert alert = new Alert(Alert.AlertType.WARNING, "Unable refresh files list.", ButtonType.OK);
+            Alert alert = new Alert(Alert.AlertType.ERROR, "Не удалось скопировать указанный файл", ButtonType.OK);
             alert.showAndWait();
         }
     }
--- src/main/java/dead/sec/kapitaz/FileManager/Main.java
+++ src/main/java/dead/sec/kapitaz/FileManager/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("/main.fxml"));
         primaryStage.setTitle("File Manager [by KapitaZz]");
-        primaryStage.setScene(new Scene(root, 800, 600));
+        primaryStage.setScene(new Scene(root, 1200, 600));
         primaryStage.show();
     }
 
